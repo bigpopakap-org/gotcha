b8ae71c58d995e7fde6a859624c8f8919062c70f
Kapil Easwar <kapil@squareup.com>
2019-10-05 09:16:52 -0700

    Got rid of a ts-ignore

--- client/src/shared.ts
+++ client/src/shared.ts
@@ -1,8 +1,9 @@
 export const GOTCHA_RESULT_QUERY_PARAM_NAME = 'gotchaResult';
 
-const GOTCHA_RESULT_VALUES = ['lose', 'extra-spin', 'gotcha-win', 'share'] as const;
+const GOTCHA_RESULT_VALUES = ['lose', 'extra-try', 'gotcha-win', 'share'] as const;
+
 export type GotchaResult = typeof GOTCHA_RESULT_VALUES[number];
+
 export function isGotchaResult(str: string | null | undefined): str is GotchaResult {
-  // @ts-ignore
-  return str && GOTCHA_RESULT_VALUES.includes(str);
+  return str ? (GOTCHA_RESULT_VALUES as readonly string[]).includes(str) : false;
 }
