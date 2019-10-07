404caa390b7e29c95234d6c67c3478f54eeee156
Kapil Easwar <kapil@squareup.com>
2019-10-07 16:11:04 -0700

    Add shared package to server and use file dependencies

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,12 @@
 import express from 'express';
+import { GOTCHA_RESULT_QUERY_PARAM_NAME } from '@gotcha/shared';
 
 const PORT = process.env.PORT || 3001;
 
 const server = express();
 
 server.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send(`Hello World!?${GOTCHA_RESULT_QUERY_PARAM_NAME}=false`);
 });
 
 server.listen(PORT, () => {
