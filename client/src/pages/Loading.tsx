60f54c475a8c5a8aa774b51d164daeaafa0e0761
Kapil Easwar <kapil@squareup.com>
2019-10-07 17:27:41 -0700

    Server-side redirection to loop through GotchaResults with a random sleep

--- client/src/pages/Loading.tsx
+++ client/src/pages/Loading.tsx
@@ -9,39 +9,11 @@ interface Props {
   currentGotchaResult: GotchaResult;
 }
 
-function advanceGotchaResult(currentGotchaResult: GotchaResult): void {
-  const queryParams = new URLSearchParams(window.location.search);
-
-  // Calculate the next gotchaResult to set
-  const nextGotchaResult = (() => {
-    switch (currentGotchaResult) {
-      case 'lose':
-        return 'extra-try';
-      case 'extra-try':
-        return 'gotcha-win';
-      case 'gotcha-win':
-        return 'share';
-      case 'share':
-        return 'share';
-      default:
-        return 'lose';
-    }
-  })();
-
-  // Set the next gotchaResult in the query params
-  queryParams.set(GOTCHA_RESULT_QUERY_PARAM_NAME, nextGotchaResult);
-  window.history.replaceState({}, document.title, `?${queryParams.toString()}`);
-}
-
 const StyledPage = styled(PageLayout)`
   background: ${props => props.theme.colors.error};
 `;
 
 export default class LoadingPage extends Component<Props, {}> {
-  componentDidMount(): void {
-    advanceGotchaResult(this.props.currentGotchaResult);
-  }
-
   render() {
     return (
       <StyledPage>
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,58 @@
 import path from 'path';
 
-import express from 'express';
-import { GOTCHA_RESULT_QUERY_PARAM_NAME } from '@gotcha/shared';
+import sleep from 'await-sleep';
+import express, { Request } from 'express';
+import { GOTCHA_RESULT_QUERY_PARAM_NAME, GotchaResult, isGotchaResult } from '@gotcha/shared';
 
 const PORT = process.env.PORT || 3001;
 const CLIENT_NODE_MODULE_PATH = path.join(__dirname, '..', 'node_modules', '@gotcha', 'client');
 
 const app = express();
 
+function getCurrentGotchaResult(req: Request): GotchaResult {
+  const gotchaResultParamValue = req.query[GOTCHA_RESULT_QUERY_PARAM_NAME];
+  if (
+    gotchaResultParamValue &&
+    typeof gotchaResultParamValue === 'string' &&
+    isGotchaResult(gotchaResultParamValue)
+  ) {
+    return gotchaResultParamValue;
+  } else {
+    return 'lose';
+  }
+}
+
+function getNextGotchaResult(currentGotchaResult: GotchaResult): GotchaResult {
+  switch (currentGotchaResult) {
+    case 'lose':
+      return 'extra-try';
+    case 'extra-try':
+      return 'gotcha-win';
+    case 'gotcha-win':
+      return 'share';
+    case 'share':
+      return 'share';
+    default:
+      return 'lose';
+  }
+}
+
 // NOTE: the URL onto which this is mounted has to match the "homepage" field on the client's package.json
 app.use('/public', express.static(path.join(CLIENT_NODE_MODULE_PATH, 'build')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(CLIENT_NODE_MODULE_PATH, 'build', 'index.html'));
+app.get('/', async (req, res) => {
+  const currentGotchaResult = getCurrentGotchaResult(req);
+
+  if (currentGotchaResult !== 'share') {
+    if (currentGotchaResult === 'gotcha-win') {
+      await sleep(1000);
+    }
+
+    const nextGotchaResult = getNextGotchaResult(currentGotchaResult);
+    res.redirect(`/?${GOTCHA_RESULT_QUERY_PARAM_NAME}=${nextGotchaResult}`);
+  } else {
+    res.sendFile(path.join(CLIENT_NODE_MODULE_PATH, 'build', 'index.html'));
+  }
 });
 
 app.get('*', (req, res) => {
